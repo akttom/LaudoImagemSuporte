ebada54f0a675ebeba972d77b12a43489de582a8
Deco Moraes <deco.moraes@icloud.com>
2023-05-22 10:57:13 -0300

    chore: change docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,17 +11,18 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://suporte1.laudoimagem.com.br',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'akttom', // Usually your GitHub org/user name.
+  projectName: 'LaudoImagemSuporte', // Usually your repo name.
+  trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn', // 'throw',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
